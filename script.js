522b1189b065b707cea82c6a52ca37ac0dbb7910
Шабанов Михаил Максимович <pl8403@C201N13.pl1.edu>
2021-04-03 09:58:33 +0500

    Second commit

--- script.js
+++ script.js
@@ -3,37 +3,60 @@ document.onkeydown = checkButton;
 let cube={
     x:0,
     y:0,
-    id:'main'
+    id:'cube',
+    width: 56,
+    height: 56,
+    trn: .25,
+    step: 100,
+    color: 'black'
 }
 
-function checkButton(e) {
+let map = {
+    width:900,
+    height:900
+}
 
-    let k=e || window.event;
+function checkButton(event) {
 
-    if (k.keyCode === 40){
+
+    if (event.keyCode === 40){
         //key: "Arrowdown"
-        cube.x +=10;
-        render(cube.id, cube.x,cube.y);  
+        
+        if (cube.x < map.width){
+            cube.x += cube.step;
+        }  
     }
-    else if (k.keyCode === 37){
+    else if (event.keyCode === 37){
         //key: "Arrowleft"
-        cube.y -=10;
-        render(cube.id, cube.x,cube.y);
+
+        if(cube.y >0 ){
+            cube.y -= cube.step;
+        } 
     }
-    else if (k.keyCode === 39){
-        // key: "Arrowright" 
-        cube.y += 10;
-        render(cube.id, cube.x,cube.y);   
+    else if (event.keyCode === 39){
+        // key: "Arrowright"
+        
+        if (cube.y < map.height){
+            cube.y += cube.step;
+        }   
     }
-    else if (k.keyCode === 38){
-        //key: "Arrowup"  
-        cube.x -=10;
-        render(cube.id, cube.x,cube.y);  
-    }             
+    else if (event.keyCode === 38){
+        //key: "Arrowup" 
+        
+        if(cube.x >0 ) {
+            cube.x -= cube.step;
+        }  
+    }   
+    render(cube);          
 }
 
-function render(id,x,y){
-    document.getElementById(id).style.top = x + 'px';
-    document.getElementById(id).style.left = y + 'px';
+function render(cube){
+    document.getElementById(cube.id).style.top = cube.x + 'px';
+    document.getElementById(cube.id).style.left = cube.y + 'px';
+    document.getElementById(cube.id).style.transition='ease'+cube.trn+'s';
+    document.getElementById(cube.id).style.width = cube.width + 'px';
+    document.getElementById(cube.id).style.height = cube.height + 'px';
+    document.getElementById(cube.id).style.backgroundColor = cube.color;
+
 }
-render(cube.id, cube.x,cube.y);
+render(cube);
